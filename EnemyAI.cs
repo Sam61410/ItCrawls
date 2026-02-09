a6ab8d0eddd166b1c4ae0081777a95d2cfb4c5a1
Sam61410 <Samuelporier@sachristian.org>
2026-02-09 13:51:32 -0600

    Enhance EnemyAI with waypoint patrol and animations

--- EnemyAI.cs
+++ EnemyAI.cs
@@ -1,41 +1,52 @@
 using StarterAssets;
 using UnityEngine;
 using UnityEngine.AI;
-using JetBrains.Annotations;
 using System.Collections;
+using System;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] FirstPersonController player;
     [SerializeField] NavMeshAgent agent;
-    public bool playerFound = false;
-
-   // public float GroundedOffset = -0.14f;
-    [SerializeField] float playerRadius = 0.28f;
-
+    [SerializeField] Animator animator;
+    //[SerializeField] ObjectGrab objectGrab;
     [SerializeField] public LayerMask PlayerLayers;
+    
+    public Transform[] waypoints;
+
+    [SerializeField] float playerRadius = 0.28f;
+    public float waitTime = 1;
+    public float waypointTolerance = 0.5f;
+
+    AnimatorControllerParameter motionSpeed;
+    public int currentPointIndex = 0;
+    private int currentWaypointIndex = 0;
+
+    public bool playerFound = false;
+    public bool once;
+    public bool loopPatrol = true;
 
     public void Update()
     {
         PlayerCheck();
-        if (!playerFound)
-        {
-            Patrol();
-        }
-        else
+        if (playerFound)
         {
             agent.SetDestination(player.transform.position);
+            agent.acceleration = 7;
+            agent.speed = 5;
+            motionSpeed.Equals(3);
         }
-    }
-    
-    public void Patrol()
-    { 
-            
-    }
-    public void FindPlayer()
-    {
-        
-    }
+        else
+        { 
+            if (!agent.pathPending && agent.remainingDistance <= waypointTolerance && !playerFound)
+            {
+                AdvanceWaypoint();
+                SetNextDestination();
+            }
+            else return;
+        }
+     }
+
     private void OnDrawGizmosSelected()
     {
         Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
@@ -46,12 +57,48 @@ public class EnemyAI : MonoBehaviour
 
         // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
         Gizmos.DrawSphere(
-            new Vector3(transform.position.x, transform.position.y, transform.position.z),playerRadius);
-
+        new Vector3(transform.position.x, transform.position.y, transform.position.z),playerRadius);
     }
     private void PlayerCheck()
     {
         Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         playerFound = Physics.CheckSphere(spherePosition, playerRadius, PlayerLayers); //,QueryTriggerInteraction.Ignore);
+    }
+
+    void Start()
+    {
+        if (waypoints == null || waypoints.Length == 0)
+        {
+            enabled = false;
+            return;
+        }
+        SetNextDestination();
+    }
+    private void SetNextDestination()
+    {
+        if (waypoints.Length == 0) return;
+
+        Transform targetWaypoint = waypoints[currentWaypointIndex];
+        if (targetWaypoint != null)
+        {
+            agent.SetDestination(targetWaypoint.position);
+            animator.Play("Crawl");
+        }
+    }
+    private void AdvanceWaypoint()
+    {
+        currentWaypointIndex++;
+
+        if (currentWaypointIndex >= waypoints.Length)
+        {
+            if (loopPatrol)
+            {
+                currentWaypointIndex = 0;
+            }
+            else
+            {
+                enabled = false; 
+            }
+        }
     }
 }
